a09537ebc7b4b7511c7804822af8277f5060b400
Ana Hoeft <ahoeft7@my.wctc.edu>
2025-03-13 08:16:31 -0500

    [2.04] Added all promts 7-9 . Deserialized the Street Fighter json. Fix spelling issue in the menu.

--- Program.cs
+++ Program.cs
@@ -48,21 +48,43 @@ else
     logger.Info($"File not found {dkFileName}");
 }
 
+// deserialize street fighter II json from file into List<StreetFighterII>
+string sf2FileName = "sf2.json";
+List<StreetFighterII> streetFighterIIs = new List<StreetFighterII>();
+if (File.Exists(sf2FileName))
+{
+    try
+    {
+        streetFighterIIs = JsonSerializer.Deserialize<List<StreetFighterII>>(File.ReadAllText(sf2FileName))!;
+        logger.Info($"File deserialized {sf2FileName}");
+    }
+    catch (Exception ex)
+    {
+        logger.Error(ex, $"Error deserializing {sf2FileName}");
+        Console.WriteLine("Error deserializing file");
+        return;
+    }
+}
+else
+{
+    logger.Info($"File not found {sf2FileName}");
+}
+
 do
 {
     // display choices to user
     Console.WriteLine("1) Display Mario Characters");
     Console.WriteLine("2) Add Mario Character");
     Console.WriteLine("3) Remove Mario Character");
-    Console.WriteLine("4) Display Donkeyharacters");
-    Console.WriteLine("5) Add Donky Knong Character");
-    Console.WriteLine("6) Remove Donky Knong Character");
-Console.WriteLine("7) Display Street Fighter II Characters");
-  Console.WriteLine("8) Add Street Fighter II Character");
-  Console.WriteLine("9) Remove Street Fighter II Character");
+    Console.WriteLine("4) Display Donkey Kong Characters");
+    Console.WriteLine("5) Add Donkey Kong Character");
+    Console.WriteLine("6) Remove Donkey Kong Character");
+    Console.WriteLine("7) Display Street Fighter II Characters");
+    Console.WriteLine("8) Add Street Fighter II Character");
+    Console.WriteLine("9) Remove Street Fighter II Character");
     Console.WriteLine("Enter to quit");
 
-    // input selection
+    // input selection from user
     string? choice = Console.ReadLine();
     logger.Info("User choice: {Choice}", choice);
 
@@ -150,6 +172,52 @@ Console.WriteLine("7) Display Street Fighter II Characters");
                 donkyKongs.Remove(character);
                 // serialize list<DonkeyKong> into json file
                 File.WriteAllText(dkFileName, JsonSerializer.Serialize(donkyKongs));
+                logger.Info($"Character Id {Id} removed");
+            }
+        }
+        else
+        {
+            logger.Error("Invalid Id");
+        }
+    }
+    else if (choice == "7")
+    {
+        // Display Street Fighter II Characters
+        foreach (var c in streetFighterIIs)
+        {
+            Console.WriteLine(c.Display());
+        }
+    }
+    else if (choice == "8")
+    {
+        // Add Street Fighter II Character
+        // Generate unique Id
+        StreetFighterII streetFighterII = new()
+        {
+            Id = streetFighterIIs.Count == 0 ? 1 : streetFighterIIs.Max(c => c.Id) + 1
+        };
+        InputCharacter(streetFighterII);
+        // Add Character
+        streetFighterIIs.Add(streetFighterII);
+        File.WriteAllText(sf2FileName, JsonSerializer.Serialize(streetFighterIIs));
+        logger.Info($"Character added: {streetFighterII.Name}");
+    }
+    else if (choice == "9")
+    {
+        // Remove Street Fighter II Character
+        Console.WriteLine("Enter the Id of the character to remove:");
+        if (UInt32.TryParse(Console.ReadLine(), out UInt32 Id))
+        {
+            StreetFighterII? character = streetFighterIIs.FirstOrDefault(c => c.Id == Id);
+            if (character == null)
+            {
+                logger.Error($"Character Id {Id} not found");
+            }
+            else
+            {
+                streetFighterIIs.Remove(character);
+                // serialize list<StreetFighterII> into json file
+                File.WriteAllText(sf2FileName, JsonSerializer.Serialize(streetFighterIIs));
                 logger.Info($"Character Id {Id} removed");
             }
         }
